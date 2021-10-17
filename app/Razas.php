b95f939dc6f6addc49533ad0309d71909ff80da9
DavidCutz <davidcutz25cona@gmail.com>
2021-10-16 21:17:08 -0500

    Modelo Razas
    modelo tabla razas

---
+++ app/Razas.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Razas extends Model
+{
+    protected $table='razas';
+    protected $primaryKey='id_razas';
+
+//definir si la llave primaria es o no un numero incrementable 
+    public $incrementing=true 
+
+    //activar o desactivar etiquetas de tiempo
+    public $timetamps=true;
+
+    public $fillable=[
+    'id_raza',
+    'raza'
+   ];
+}
